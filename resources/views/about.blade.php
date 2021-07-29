e5a8c02661198114845bc4afce015bced547b591
Carlos André <carlos.andre1270@gmail.com>
2021-07-29 12:55:44 -0300

    informações da página sobre e ajustes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -13,30 +13,38 @@
                   <div class="form-row">
                       <div class="col-md-12">
                           <div class="form-group">
-                              <h4 style="font-weight:normal">Apresentação:</h4>
-                              <h6 style="font-weight: normal; text-align:justify; color:#727272">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h6>
+                                <h4 style="font-weight:normal">Apresentação:</h4>
+                                <h6 style="font-weight: normal; text-align:justify; color:#727272">
+                                    O sistema UFAPE Concursos surgiu a partir de uma demanda da própria Universidade com o objetivo de viabilizar uma ferramenta para gerenciar o processo de inscrição, recebimento de documentos, avaliação e atribuição de notas pela banca examinadora.
+                                </h6>
                           </div>
                       </div>
                       <div class="col-md-6">
                         <div class="form-group">
                             <h4 style="font-weight:normal">Principais funcionalidades:</h4>
                             <ul style="margin-left: -20px">
-                                <li style="margin-top:15px; color:#727272">
+                                <li>
                                     <div class="btn-group">
-                                        <div style="margin-top:5px; margin-right:5px"><img src="{{ asset('img/icon_ponto.svg') }}" width="5px"></div>
-                                        <div><h6>Funcionalidade 1</h6></div>
+                                        <div style="margin-top:5px; margin-right:5px"></div>
+                                        <div><h6>Cadastro de concursos.</h6></div>
                                     </div>
                                 </li>
-                                <li style="color:#727272">
+                                <li>
                                     <div class="btn-group">
-                                        <div style="margin-top:5px; margin-right:5px"><img src="{{ asset('img/icon_ponto.svg') }}" width="5px"></div>
-                                        <div><h6>Funcionalidade 2</h6></div>
+                                        <div style="margin-top:5px; margin-right:5px"></div>
+                                        <div><h6>Inscrição de candidatos.</h6></div>
                                     </div>
                                 </li>
-                                <li style="margin-bottom: 15px; color:#727272">
+                                <li>
                                     <div class="btn-group">
-                                        <div style="margin-top:5px; margin-right:5px"><img src="{{ asset('img/icon_ponto.svg') }}" width="5px"></div>
-                                        <div><h6>Funcionalidade 3</h6></div>
+                                        <div style="margin-top:5px; margin-right:5px"></div>
+                                        <div><h6>Envio de documentação.</h6></div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="btn-group">
+                                        <div style="margin-top:5px; margin-right:5px"></div>
+                                        <div><h6>Avaliação de documentos submetidos.</h6></div>
                                     </div>
                                 </li>
                             </ul> 
@@ -45,13 +53,13 @@
                     <div class="col-md-12">
                         <div class="form-group">
                             <h4 style="font-weight:normal">Registro:</h4>
-                            <a href="">Número de Registro</a>
+                            <h6 style="font-weight: normal">Em definição.</h6>
                         </div>
                     </div>
                     <div class="col-md-12">
                         <div class="form-group">
                             <h4 style="font-weight:normal">Código-fonte:</h4>
-                            <a href="">Nome do sistema</a>
+                            <a target="_blanck" href="https://github.com/lmts-ufape/concursos-ufape">GitHub</a>
                         </div>
                     </div>
                     <div class="col-md-12">
@@ -63,60 +71,66 @@
                     <div class="col-md-6">
                         <div class="form-group">
                             <h4 style="font-weight:normal">Equipe:</h4>
-                            <h6 style="font-weight:normal; color:#727272">Docentes:</h6>
+                            <h6 style="font-weight:normal;">Docentes:</h6>
                             <div>
-                                <ul style="margin-left: -20px">
+                                <ul style="margin-left: -20px" style="list-style-type: none;">
                                     <li style="margin-top:15px; margin-bottom: 15px">
                                         <div class="btn-group">
-                                            <div style="margin-right:10px"><img src="{{ asset('img/icon_ponto.svg') }}" width="5px"></div>
-                                            <div><a href="">Dr. Anderson Fernandes de Alencar</a></div>
+                                            <div style="margin-right:10px"></div>
+                                            <div><a href="http://lattes.cnpq.br/9517716593738845" target="_blanck">Dr. Anderson Fernandes de Alencar</a></div>
                                         </div>
                                     </li>
                                     <li style="margin-top:15px; margin-bottom: 15px">
                                         <div class="btn-group">
-                                            <div style="margin-right:10px"><img src="{{ asset('img/icon_ponto.svg') }}" width="5px"></div>
-                                            <div><a href="">Dr. Rodrigo Gusmão de Carvalho Rocha</a></div>
+                                            <div style="margin-right:10px"></div>
+                                            <div><a href="http://lattes.cnpq.br/4654692334430085" target="_blanck">Dr. Rodrigo Gusmão de Carvalho Rocha</a></div>
                                         </div>
                                     </li>
                                     <li style="margin-top:15px; margin-bottom: 15px">
                                         <div class="btn-group">
-                                            <div style="margin-right:10px"><img src="{{ asset('img/icon_ponto.svg') }}" width="5px"></div>
-                                            <div><a href="">Dr. Igor Medeiros Vanderlei</a></div>
+                                            <div style="margin-right:10px"></div>
+                                            <div><a href="http://lattes.cnpq.br/7448139435512224" target="_blanck">Dr. Igor Medeiros Vanderlei</a></div>
                                         </div>
                                     </li>
                                     <li style="margin-top:15px; margin-bottom: 15px">
                                         <div class="btn-group">
-                                            <div style="margin-right:10px"><img src="{{ asset('img/icon_ponto.svg') }}" width="5px"></div>
-                                            <div><a href="">Dr. Jean Carlos Teixeira de Araujo</a></div>
+                                            <div style="margin-right:10px"></div>
+                                            <div><a href="http://lattes.cnpq.br/2498961747789618" target="_blanck">Dr. Jean Carlos Teixeira de Araujo</a></div>
                                         </div>
                                     </li>
                                     <li style="margin-top:15px; margin-bottom: 15px">
                                         <div class="btn-group">
-                                            <div style="margin-right:10px"><img src="{{ asset('img/icon_ponto.svg') }}" width="5px"></div>
-                                            <div><a href="">Dr. Mariel José Pimentel de Andrade</a></div>
+                                            <div style="margin-right:10px"></div>
+                                            <div><a href="http://lattes.cnpq.br/3111765717865989" target="_blanck">Dr. Mariel José Pimentel de Andrade</a></div>
                                         </div>
                                     </li>
                                 </ul> 
                             </div>
-                            <h6 style="font-weight:normal; color:#727272">Desenvolvedores:</h6>
+                            <h6 style="font-weight:normal;">Desenvolvedores:</h6>
                             <div>
                                 <ul style="margin-left: -20px">
                                     <li style="margin-top:15px; margin-bottom: 15px">
                                         <div class="btn-group">
-                                            <div style="margin-right:10px"><img src="{{ asset('img/icon_ponto.svg') }}" width="5px"></div>
-                                            <div><a href="">Fulano</a></div>
+                                            <div style="margin-right:10px"></div>
+                                            <div><a href="https://www.linkedin.com/in/carlos-andr%C3%A9-611766196/" target="_blanck">Carlos André de Almeida Cavalcante</a></div>
                                         </div>
                                     </li>
                                     <li style="margin-top:15px; margin-bottom: 15px">
                                         <div class="btn-group">
-                                            <div style="margin-right:10px"><img src="{{ asset('img/icon_ponto.svg') }}" width="5px"></div>
-                                            <div><a href="">Fulano</a></div>
+                                            <div style="margin-right:10px"></div>
+                                            <div><a href="https://www.linkedin.com/in/danillo-bion-92b8aa193/" target="_blanck">Danillo José Miranda Bion de Lima</a></div>
                                         </div>
                                     </li>
                                     <li style="margin-top:15px; margin-bottom: 15px">
                                         <div class="btn-group">
-                                            <div style="margin-right:10px"><img src="{{ asset('img/icon_ponto.svg') }}" width="5px"></div>
-                                            <div><a href="">Fulano</a></div>
+                                            <div style="margin-right:10px"></div>
+                                            <div><a href="https://br.linkedin.com/in/kelwin-jonas-1b8656214" target="_blanck">Kelwin Jonas Silva Santos</a></div>
+                                        </div>
+                                    </li>
+                                    <li style="margin-top:15px; margin-bottom: 15px">
+                                        <div class="btn-group">
+                                            <div style="margin-right:10px"></div>
+                                            <div><a href="https://www.linkedin.com/in/lu%C3%ADs-fernando-b6538518a" target="_blanck">Luís Fernando Rocha Lima</a></div>
                                         </div>
                                     </li>
                                 </ul> 
